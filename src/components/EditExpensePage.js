5de35d7721ba28eb6a461f084fd3e091be1dc524
Mario Borras <mario.borras@icloud.com>
2019-01-09 21:26:03 -0500

    learning redux

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-const EditExpensePage = () => <div>This is the EditPage Component</div>;
+const EditExpensePage = props => <div>Editing the expense with id of {props.match.params.id}</div>;
 
 export default EditExpensePage;
