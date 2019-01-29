e66f5dd3a907e4efcf2af86a46853740062b13ed
Mario Borras <mario.borras@icloud.com>
2019-01-29 15:50:50 -0500

    refactored redux

---
+++ src/actions/filters.js
@@ -0,0 +1,21 @@
+//SET_TEXT_FILTERS
+export const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+});
+//SORT_BY_DATE
+export const sortByDate = () => ({
+  type: 'SORT_BY_DATE'
+});
+//SORT_BY_AMOUNT
+export const sortByAmount = () => ({
+  type: 'SORT_BY_AMOUNT'
+});
+export const setStartDate = (startDate = undefined) => ({
+  type: 'SET_START_DATE',
+  startDate
+});
+export const setEndDate = (endDate = undefined) => ({
+  type: 'SET_END_DATE',
+  endDate
+});
--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import AppRouter from "./routers/AppRouter";
-import "normalize.css/normalize.css";
-import "./styles/styles.scss";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 
-ReactDOM.render(<AppRouter />, document.querySelector("#app"));
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'water bill', amount: 100, createdAt: -21000 }));
+store.dispatch(addExpense({ description: 'gas bill', amount: 30, createdAt: 0 }));
+store.dispatch(setTextFilter('water'));
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+ReactDOM.render(<AppRouter />, document.querySelector('#app'));
