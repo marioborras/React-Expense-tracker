29e424affc38b550549016b211b3cbcc9bcf9330
Mario Borras <mario.borras@icloud.com>
2019-03-13 16:08:26 -0400

    added jest testing

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,25 +1,27 @@
 import uuid from 'uuid';
 
-//ADD_EXPENSE
-export const addExpense = ({ description = '', note = '', amount = 0, createdAt = 0 } = {}) => ({
+// ADD_EXPENSE
+export const addExpense = ({
+  description = '', note = '', amount = 0, createdAt = 0,
+} = {}) => ({
   type: 'ADD_EXPENSE',
   expense: {
     id: uuid(),
     description,
     note,
     amount,
-    createdAt
-  }
+    createdAt,
+  },
 });
-//REMOVE_EXPENSE
+// REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
-  id
+  id,
 });
 
-//EDIT_EXPENSE
+// EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
   id,
-  updates
+  updates,
 });
--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,8 +1,8 @@
 import moment from 'moment';
 // Get visibile expenses
 
-export default (expenses, { text, sortBy, startDate, endDate }) => {
-  return expenses
+export default (expenses, { text, sortBy, startDate, endDate }) =>
+  expenses
     .filter(expense => {
       const createdAtMoment = moment(expense.createdAt);
       const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
@@ -13,8 +13,8 @@ export default (expenses, { text, sortBy, startDate, endDate }) => {
     .sort((a, b) => {
       if (sortBy === 'date') {
         return a.createdAt < b.createdAt ? 1 : -1;
-      } else if (sortBy === 'amount') {
+      }
+      if (sortBy === 'amount') {
         return a.amount < b.amount ? 1 : -1;
       }
     });
-};
